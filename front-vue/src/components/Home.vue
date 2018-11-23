a797af70d2ef4434394c58813a30bd3310ceaa6c
Matheus <mihertzog@inf.ufpel.edu.br>
2018-11-23 15:59:19 -0200

    Home page basics

--- front-vue/src/components/Home.vue
+++ front-vue/src/components/Home.vue
@@ -1,24 +1,59 @@
 <template>
-  <div class="home">
-  <p>
-    {{name}}
-  </p>
-  <div class="dropdown-menu">
-    <router-link v-bind:to="{ name: 'Posts' }" class="">
-      <a class="dropdown-item">Solicitações</a>
-    </router-link>
-    <a class="dropdown-item" href="#">FAQ</a>
-    <a class="dropdown-item" href="#">Sair</a>
-    <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
+  <div class="page-container md-layout-row home">
+
+    <md-app>
+  <md-app-toolbar class="md-primary">
+    <span class="md-title">Header</span>
+  </md-app-toolbar>
+
+  <md-app-drawer md-permanent="full">
+      <div class="centered-container">
+        <md-content class="md-elevation-12 user-pic">
+            <img src="https://orig00.deviantart.net/adc4/f/2018/201/f/0/profile_picture_by_dummy_doodles-dchro9m.png"></img>
+        </md-content>
+      </div>
+
+    <md-list>
+
+      <md-list-item>
+        <router-link v-bind:to="{ name: 'Posts' }" class="" style="width:100%">
+        <md-button class="md-raised md-primary" style="width:100%">HOME</md-button>
+      </router-link>
+      </md-list-item>
+
+      <md-list-item>
+        <router-link v-bind:to="{ name: 'Posts' }" class="" style="width:100%">
+        <md-button class="md-raised md-primary" style="width:100%">SOLICITAÇÕES</md-button>
+      </router-link>
+      </md-list-item>
+
+      <md-list-item>
+        <router-link v-bind:to="{ name: 'Posts' }" class="" style="width:100%">
+        <md-button class="md-raised md-primary" style="width:100%">FAQ</md-button>
+      </router-link>
+      </md-list-item>
+
+      <md-list-item>
+        <router-link v-bind:to="{ name: 'Posts' }" class="" style="width:100%">
+        <md-button class="md-raised md-primary" style="width:100%">LINK --  </md-button>
+      </router-link>
+      </md-list-item>
+
+    </md-list>
+  </md-app-drawer>
+
+  <md-app-content>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.
+  </md-app-content>
+</md-app>
+
   </div>
 </template>
 
 <script>
 import PostsService from '@/services/PostsService'
 export default {
-  name: 'home',
+  name: 'PermanentFull',
   data () {
     return {
       name: 'None',
@@ -41,4 +76,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.md-app {
+  min-height: 350px;
+  border: 1px solid rgba(#000, .12);
+}
+
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+.user-pic{
+  width: 50%;
+  margin-top: 35px;
+  margin-bottom: 35px;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
