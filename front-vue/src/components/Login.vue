e6752246f670ab717464220ff3822c4216ffb742
Matheus <mihertzog@inf.ufpel.edu.br>
2018-11-23 13:41:00 -0200

    Added sass-loader & vue-material support & built login page

--- front-vue/src/components/Login.vue
+++ front-vue/src/components/Login.vue
@@ -1,48 +1,126 @@
 <template>
-  <div class='login'>
-    <h2>Login</h2>
-    <p v-if="$route.query.redirect">
-      You need to login first.
-    </p>
-    <form @submit.prevent="login" autocomplete="off">
-      <label><input v-model="email" placeholder="email" v-focus></label>
-      <label><input v-model="pass" placeholder="password" type="password"></label><br>
-      <router-link v-bind:to="{ name: 'Home' }" class="">
-        <button type="submit">login</button>
-      </router-link>
-      <p v-if="error" class="error">Bad login information</p>
-    </form>
+  <div class="centered-container">
+    <md-content class="md-elevation-8">
+
+      <div class="title">
+        <img src="https://vuematerial.io/assets/logo-color.png">
+        <div class="md-title">INSERT LOGO ABOVE</div>
+        <div class="md-body-1">Login</div>
+      </div>
+
+      <p v-if="$route.query.redirect">
+          You need to login first.
+      </p>
+      <div class="form">
+      <form @submit.prevent="login" autocomplete="off">
+        <md-field>
+          <label>E-mail</label>
+          <md-input v-model="login.email" autofocus></md-input>
+        </md-field>
+
+        <md-field md-has-password>
+          <label>Password</label>
+          <md-input v-model="login.password" type="password"></md-input>
+        </md-field>
+         <p v-if="error" class="error">Bad login information</p>
+        </form>
+      </div>
+
+      <div class="actions md-layout md-alignment-center-space-between">
+        <a href="/resetpassword">Reset password</a>
+
+        <router-link v-bind:to="{ name: 'Home' }" class="">
+        <md-button class="md-raised md-primary" @click="auth">Log in</md-button>
+        </router-link>
+        <p v-if="error" class="error">Bad login information</p>
+      </div>
+
+      <div class="loading-overlay" v-if="loading">
+        <md-progress-spinner md-mode="indeterminate" :md-stroke="2"></md-progress-spinner>
+      </div>
+
+    </md-content>
+    <div class="background" />
   </div>
 </template>
 
 <script>
-  //import auth from '../auth'
-
-  export default {
-    data () {
-      return {
-        email: '',
-        pass: '',
-        error: false
+export default {
+  name: "App",
+  data() {
+    return {
+      loading: false,
+      login: {
+        email: "",
+        password: ""
       }
-    },
-    methods: {
-      login () {
-        // auth.login(this.email, this.pass, loggedIn => {
-        //   if (!loggedIn) {
-        //     this.error = true
-        //   } else {
-        //     this.$router.replace(this.$route.query.redirect || '/')
-        //   }
-        // })
-
-      }
+    };
+  },
+  methods: {
+    auth() {
+      // your code to login user
+      // this is only for example of loading
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 5000);
     }
   }
+};
 </script>
 
-<style>
-  .error {
-    color: red;
+<style lang="scss">
+.centered-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  .title {
+    text-align: center;
+    margin-bottom: 30px;
+    img {
+      margin-bottom: 16px;
+      max-width: 80px;
+    }
   }
+  .actions {
+    .md-button {
+      margin: 0;
+    }
+  }
+  .form {
+    margin-bottom: 60px;
+  }
+  .background {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 0;
+  }
+  .md-content {
+    z-index: 1;
+    padding: 40px;
+    width: 100%;
+    max-width: 400px;
+    position: relative;
+  }
+  .loading-overlay {
+    z-index: 10;
+    top: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
 </style>
--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -6,6 +6,12 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
