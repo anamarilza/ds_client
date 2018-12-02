163e0f0b96ac2754e574a3c545c7ba73ac3883be
Matheus <mihertzog@inf.ufpel.edu.br>
2018-12-02 14:42:01 -0200

    working on post page

--- front-vue/src/components/Posts.vue
+++ front-vue/src/components/Posts.vue
@@ -1,45 +1,150 @@
 <template>
-  <div class="posts">
-    // This is all the data being received //
-    
-    {{requests}}
-    <h1>Posts</h1>
-    <div v-if="requests.length > 0" class="table-wrap">
-      <div>
-        <router-link v-bind:to="{ name: 'NewPost' }" class="">Add Post</router-link>
+  <div class="page-container md-layout-row home">
+
+    <md-app>
+  <md-app-toolbar class="md-primary">
+    <span class="md-title">Header</span>
+  </md-app-toolbar>
+
+  <md-app-drawer md-permanent="clipped">
+      <div class="userimg-container">
+        <router-link v-bind:to="{ name: 'UserProfile' }">
+        <md-content class="md-elevation-12 user-pic">
+            <img src="https://orig00.deviantart.net/adc4/f/2018/201/f/0/profile_picture_by_dummy_doodles-dchro9m.png"></img>
+        </md-content>
+        <p>
+          {{name}}
+        </p>
+      </router-link>
       </div>
-      <table>
-        <tr>
-          <td>Title</td>
-          <td width="550">Description</td>
-          <td width="100" align="center">Action</td>
-        </tr>
-        <tr v-for="post in requests" :key="post.id_solicitacao">
-          <td>{{ post.id_solicitacao }}</td>
-          <td>{{ post.data_solic }}</td>
-          <td align="center">
-            <router-link v-bind:to="{ name: 'EditPost', params: { id: post._id } }">Edit</router-link> |
-            <a href="#" @click="deletePost(post._id)">Delete</a>
-          </td>
-        </tr>
-      </table>
-    </div>
-    <div v-else>
-      There are no posts.. Lets add one now <br /><br />
-      <router-link v-bind:to="{ name: 'NewPost' }" class="add_post_link">Add Post</router-link>
-    </div>
+      <div class="nav-container">
+
+        <router-link v-bind:to="{ name: 'Home' }">
+          <md-button class="md-raised md-primary">HOME</md-button>
+      </router-link>
+
+
+
+        <router-link v-bind:to="{ name: 'Posts' }" >
+        <md-button class="md-raised md-primary" >SOLICITAÇÕES</md-button>
+      </router-link>
+
+
+
+        <router-link v-bind:to="{ name: 'FAQ' }" >
+        <md-button class="md-raised md-primary" >FAQ</md-button>
+      </router-link>
+
+
+
+        <router-link v-bind:to="{ name: 'Posts' }" class="" >
+        <md-button class="md-raised md-primary">LINK --  </md-button>
+      </router-link>
+
+
   </div>
+  </md-app-drawer>
+
+
+  <md-app-content class="main-content-holder">
+
+
+    <div id="requests-container">
+      <ul>
+          <li v-for="request in requests">
+            {{ request.id_solicitacao }}
+            {{ request.nome_aluno }}
+            {{ request.id_atividade }}
+            {{ request.horas_info }}
+            {{ request.data_solic }}
+            <br>
+          </li>
+      </ul>
+
+    </div>
+
+
+
+
+
+
+
+    <!-- POST CONTENT HERE -->
+    <div class="posts">
+      // This is all the data being received //
+
+      {{requests}}
+      <h1>Posts</h1>
+      <div v-if="requests.length > 0" class="table-wrap">
+        <div>
+          <router-link v-bind:to="{ name: 'NewPost' }" class="">Add Post</router-link>
+        </div>
+        <table>
+          <tr>
+            <td>Title</td>
+            <td width="550">Description</td>
+            <td width="100" align="center">Action</td>
+          </tr>
+          <tr v-for="post in requests" :key="post.id_solicitacao">
+            <td>{{ post.id_solicitacao }}</td>
+            <td>{{ post.data_solic }}</td>
+            <td align="center">
+              <router-link v-bind:to="{ name: 'EditPost', params: { id: post._id } }">Edit</router-link> |
+              <a href="#" @click="deletePost(post._id)">Delete</a>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div v-else>
+        There are no posts.. Lets add one now <br /><br />
+        <router-link v-bind:to="{ name: 'NewPost' }" class="add_post_link">Add Post</router-link>
+      </div>
+    </div>
+
+
+
+
+
+
+
+  </md-app-content>
+  </md-app>
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
 import HoursService from '@/services/HoursService'
+
+const toLower = text => {
+  return text.toString().toLowerCase()
+}
+
+const searchByName = (items, term) => {
+  if (term) {
+    return items.filter(item => toLower(item.name).includes(toLower(term)))
+  }
+
+  return items
+}
 export default {
   name: 'posts',
-  data () {
-    return {
-      requests: []
-    }
-  },
+  data: () => ({
+      requests : [],
+      search: null,
+      searched: []
+    }),
   mounted () {
     this.fetchRequests()
   },
@@ -48,41 +153,54 @@ export default {
       const response = await HoursService.fetchRequests({id:0})
       this.requests = response.data
     },
-  }
+}
 }
 </script>
+
+
+
 <style type="text/css">
-.table-wrap {
-  width: 60%;
-  margin: 0 auto;
-  text-align: center;
+
+.userimg-container{
+  max-height: 350px;
 }
-table th, table tr {
-  text-align: left;
+.nav-container{
+  max-height: 350px;
 }
-table thead {
-  background: #f2f2f2;
+.chart-holder{
+  padding-top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1200px;
 }
-table tr td {
-  padding: 10px;
+.router-link{
+  width:100%;
 }
-table tr:nth-child(odd) {
-  background: #f2f2f2;
+.md-button{
+  border: 10px solid rgba(255,255,255,.3);
+  border-style: solid;
+  border-width: 1px;
+  width: 90%;
 }
-table tr:nth-child(1) {
-  background: #4d7ef7;
-  color: #fff;
+.md-app {
+  min-height: 350px;
+  border: 1px solid rgba(#000, .12);
 }
-a {
-  color: #4d7ef7;
-  text-decoration: none;
+
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+  max-height: auto;
 }
-a.add_post_link {
-  background: #4d7ef7;
-  color: #fff;
-  padding: 10px 80px;
-  text-transform: uppercase;
-  font-size: 12px;
-  font-weight: bold;
+.user-pic{
+  border: 10px solid rgba(255,255,255,.7);
+  width: 50%;
+  margin-top: 35px;
+  margin-bottom: 35px;
+  margin-left: auto;
+  margin-right: auto;
 }
+.md-field {
+    max-width: 300px;
+  }
 </style>
